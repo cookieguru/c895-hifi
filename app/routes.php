2011cbedae10267bc24d3886305775e935508187
Garth Kerr <garthkerr@gmail.com>
2015-09-10 19:12:20 -0400

    fixes

--- app/routes.php
+++ app/routes.php
@@ -24,4 +24,4 @@ Route::get('/login', array('uses' => 'HomeController@showLogin'));
 Route::post('/login', array('uses' => 'HomeController@doLogin'));
 
 // route to logout
-Route::get('/logout', array('uses' => 'HomeController@doLogout));
+Route::get('/logout', array('uses' => 'HomeController@doLogout'));
