f889c155a1be1eda8c78b98c95b50ad63ca907bf
Barrett <ghoastfalcon@gmail.com>
2015-09-10 17:37:09 -0700

    Temp fix for reasons.

--- app/routes.php
+++ app/routes.php
@@ -26,9 +26,7 @@ Route::post('/login', array('uses' => 'HomeController@doLogin'));
 // route to logout
 Route::get('/logout', array('uses' => 'HomeController@doLogout'));
 
-if(Auth::check()) {
-	Route::get('/upload/', array('uses' => 'UploadController@mainAction'));
-	Route::post('/upload', array('uses' => 'UploadController@doAction'));
-}
+Route::get('/upload/', array('uses' => 'UploadController@mainAction'));
+Route::post('/upload', array('uses' => 'UploadController@doAction'));
 
 Route::get('/list', array('uses' => 'UploadController@listAction'));
