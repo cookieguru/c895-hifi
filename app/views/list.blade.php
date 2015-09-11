8849c0363d5eb08dd4acc02115e338ae83422a74
Cookie Guru <cookieguru@gmail.com>
2015-09-10 20:34:52 -0700

    Full width for better layout

--- app/views/list.blade.php
+++ app/views/list.blade.php
@@ -75,7 +75,7 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-lg-6">
+			<div class="col-lg-12">
 				<div class="panel panel-default">
 					<div class="panel-body">
 						<div class="row">
