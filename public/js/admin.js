45e900cc238cbd7434bd46395a8b7910be114243
Cookie Guru <cookieguru@gmail.com>
2015-09-10 20:32:18 -0700

    Lazy file detection; fix ability to create multiple audio players

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,7 @@
 $(function() {
 	$('.play').click(function() {
-		$('<audio controls><source src=\"' + this.href + '\" type=\"audio/mp4a\"></audio>').insertAfter(this);
+		$('<audio controls><source src=\"' + this.href + '\" type=\"audio/' + this.href.substr(-3) + '\"></audio>').insertAfter(this);
+		$(this).remove();
 		return false
 	})
 });
